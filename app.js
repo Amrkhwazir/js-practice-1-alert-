84d3706d43539ffb6c2642439aa14a2014c0e730
Amirkhan567 <amrkh5678@gmail.com>
2023-01-28 22:43:57 +0500

    practice1

---
+++ app.js
@@ -0,0 +1,23 @@
+alert("Assalam o alikum")
+var name = "Amir"
+alert(name)
+var nationality = "Pakistani"
+alert (nationality)
+var age = 22
+alert(age)
+var weight = 55
+alert (weight)
+var total = num1 + num2
+alert(total)
+var total = num1 - num2
+alert(total)
+var total = num1 * num2
+alert(total)
+var total = num1 / num2
+alert(total)
+var total = num1 % num2
+alert(total)
+var num1 = 30
+var num2 = 50
+var percnt = num1 * 100 / num2
+alert(percnt)
